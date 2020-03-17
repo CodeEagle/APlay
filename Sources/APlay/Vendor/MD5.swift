3da306ea9d38381e01a0b66188190e7e2a2e3ef2
CodeEagle <Stasura@hotmail.com>
2020-03-17 16:28:12 +0800

    change: optimize MD5 algorithm

--- Sources/APlay/Vendor/MD5.swift
+++ Sources/APlay/Vendor/MD5.swift
@@ -1,16 +1,8 @@
-import typealias CommonCrypto.CC_LONG
-import func CommonCrypto.CC_MD5
-import var CommonCrypto.CC_MD5_DIGEST_LENGTH
-import Foundation
+import CryptoKit
 
 extension Data {
-    var md5: String {
-        let data = self
-        let hash = data.withUnsafeBytes { (bytes: UnsafeRawBufferPointer) -> [UInt8] in
-            var hash = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
-            CC_MD5(bytes.baseAddress, CC_LONG(data.count), &hash)
-            return hash
-        }
-        return hash.map { String(format: "%02x", $0) }.joined()
+    public var md5: String {
+        let val = Insecure.MD5.hash(data: self)
+        return val.description.replacingOccurrences(of: "MD5 digest: ", with: "")
     }
 }
